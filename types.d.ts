10e4e386a2384ec23231c77e7fc498b2b33d2904
cliid <cliid@protonmail.com>
2021-10-23 14:45:35 +0900

    changed types

--- types.d.ts
+++ types.d.ts
@@ -2,3 +2,14 @@ declare module 'smartquotes' {
   export default function _(context: any): any;
   export function string(context: string): string;
 }
+
+interface RehypeProbeImageSizeOptions {
+  /**
+   * For local images, you can specify the image directory.
+   */
+  staticDir?: string;
+}
+
+declare module 'rehype-probe-image-size' {
+  export default function _(): Plugin<[RehypeProbeImageSizeOptions?]>;
+}
