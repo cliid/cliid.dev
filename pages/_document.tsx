eabec4b76fcfe38b0c14302ec864b5193056fa0a
cliid <cliid@protonmail.com>
2021-10-26 21:35:16 +0900

    optimized font loading strategy

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <link rel="dns-prefetch" href="//fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700;900|JetBrains+Mono:wght@400&display=swap"
