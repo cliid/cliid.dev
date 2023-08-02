4f0470840ad6f89e469217138c70d7b3cf5ea18d
Jiwu Jang <cliid@pm.me>
2023-08-02 09:47:41 +0900

    change a bit

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,13 @@
 import NextAuth, { NextAuthOptions } from 'next-auth';
+import FacebookProvider from 'next-auth/providers/facebook';
 import GithubProvider from 'next-auth/providers/github';
 
 export const authOptions: NextAuthOptions = {
   providers: [
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID ?? '',
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET ?? ''
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_KEY ?? '',
       clientSecret: process.env.GITHUB_CLIENT_SECRET ?? ''
